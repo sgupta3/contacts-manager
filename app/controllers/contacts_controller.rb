c976619fc45c75b74815ae0efea2f6acfb5c42e5
Sahil Gupta <emailsahilgupta12@gmail.com>
2015-05-04 16:50:18 -0400

    CRUD functionality added for Contacts including Gravatar image for the contact page

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,48 @@
+class ContactsController < ApplicationController
+	def index
+		@contacts = Contact.all
+	end
+
+	def show
+		@contact = Contact.find(params[:id])
+		@gravatar_email_hash = Digest::MD5.hexdigest(@contact.email)
+	end
+
+	def new
+		@contact = Contact.new
+	end
+
+	def edit
+		@contact = Contact.find(params[:id])
+	end
+
+	def create
+		@contact = Contact.new(contact_params)
+
+		@contact.save
+		redirect_to @contact
+	end
+
+	def update
+		@contact = Contact.find(params[:id])
+
+		if @contact.update(contact_params)
+    		redirect_to @contact
+    	end
+	end
+
+	def destroy 
+		@contact = Contact.find(params[:id])
+		@contact.destroy
+		redirect_to contacts_path
+	end
+
+	
+
+	
+
+	private
+		def contact_params
+			params.require(:contact).permit(:first_name,:last_name,:email,:phone)
+		end
+end
